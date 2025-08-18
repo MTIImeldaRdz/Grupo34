1a551654b6804b27499fdba4bb440da1f5a2058e
MTIImeldaRdz <imelda.rodriguezrm@uanl.edu.mx>
2025-08-18 16:23:35 -0600

    Mi primer archivo

--- listas.py
+++ listas.py
@@ -1,2 +1,16 @@
-lista1 = ['a', 'b', 'c'] 
-print(lista1)
+cantidad = 0
+i = 0
+ciudades = []   
+
+cantidad = int(input("Ingrese la cantidad de ciudades que desea agregar: "))
+
+if cantidad > 0:
+    while i < cantidad:
+        ciudad = input("Ingrese el nombre de la ciudad: ")
+        ciudades.append(ciudad)
+        i += 1  
+    print("Las ciudades ingresadas son:")
+    for ciudad in ciudades:
+        print(ciudad)       
+else:
+    print("La cantidad debe ser mayor a 0.")
